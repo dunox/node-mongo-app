f571c1bd77dd2e9eda63f65a550d5bc0caca2ad3
front.andrey.sh <front.andrey.sh@alldigitalads.com>
2021-03-17 10:37:55 +0200

    add and update in db

--- routes/todos.js
+++ routes/todos.js
@@ -4,7 +4,7 @@ const router = Router();
 
 
 router.get('/', async (req, res) => {
-    const todos = await Todo.find({})
+    const todos = await Todo.find({}).lean();
 
     res.render('index', {
         title: 'Todos List',
@@ -20,6 +20,26 @@ router.get('/create', (req, res) => {
         isCreate: true
     })
 });
+
+
+router.post('/create', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    });
+
+    await todo.save();
+
+    res.redirect('/');
+});
+
+router.post('/complete', async (req, res) => {
+    const todo = await Todo.findById(req.body.id);
+
+    todo.completed = !!req.body.completed;
+    await todo.save();
+
+    res.redirect('/');
+})
 
 module.exports = router;
 
